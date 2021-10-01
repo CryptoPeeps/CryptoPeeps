633cd5e84e841261cd2a219f62ebb51168af544e
Benzega <benzega@protonmail.com>
2021-10-01 01:34:32 -0400

    perf: rewrite/restructure token metadata (The Great ReMinting)

--- src/updateArtworkMetadata.js
+++ src/updateArtworkMetadata.js
@@ -219,6 +219,146 @@ const createArtworkMetadata = (
     });
 }
 
+
+
+const cleanFilesOut = (
+    species,
+    variant,
+    opts = {
+        test: true,
+        range: [1, 1],
+    }
+) => {
+
+    const dir = __dirname + `/../collections/${species}/${variant}`;
+
+    let arr = fs.readdirSync(dir)
+        .filter(aN => Number.isInteger(parseInt(aN)))
+        .filter(aN => parseInt(aN))
+        .sort((a, b) => a - b);
+
+    if (opts.test) {
+        arr = arr.filter(aN => aN >= opts.range[0] && aN <= opts.range[1]);
+    }
+
+    arr.filter(aN => {
+        if (Number.isInteger(parseInt(aN))) {
+            if (opts.test) {
+                inspect(`${aN}`)
+            } else {
+
+
+                if (fs.existsSync(dir + `/${aN}/tx.json`)) {
+                    fs.rmSync(dir + `/${aN}/tx.json`)
+                    fs.rmSync(dir + `/${aN}/metadata.json`)
+                }
+
+
+                console.log(aN);
+                fs.renameSync(dir + `/${aN}/artwork.chunked.final.json`, dir + `/${aN}/artwork.chunked.v2.json`)
+                fs.writeFileSync(dir + `/${aN}/artwork.chunked.final.json`, JSON.stringify(md["new"]));
+            }
+        }
+    });
+}
+
+/** ====================================================================================
+ * Create final JSON
+ *
+ * @since 1.0
+ */
+const createFinalArtworkMetadata = (
+    species,
+    variant,
+    opts = {
+        title: "artwork.final.json",
+        test: true,
+        range: [1, 1],
+    }
+) => {
+    const dir = __dirname + `/../collections/${species}/${variant}`;
+
+    let arr = fs.readdirSync(dir)
+        .filter(aN => Number.isInteger(parseInt(aN)))
+        .filter(aN => parseInt(aN))
+        .sort((a, b) => a - b);
+
+    if (opts.test) {
+        arr = arr.filter(aN => aN >= opts.range[0] && aN <= opts.range[1]);
+    }
+
+    arr.filter(aN => {
+        if (Number.isInteger(parseInt(aN))) {
+
+            // Get old metadata
+            let md = JSON.parse(fs.readFileSync(dir + `/${aN}/artwork.chunked.final.json`))
+
+            // Define svg with both types of encoding
+            let svg = {
+                base: fs.readFileSync(dir + `/${aN}/artwork.svg`, `utf8`),
+                encoded: fs.readFileSync(dir + `/${aN}/artwork.svg`, 'base64')
+            }
+
+            // Produce srcArr from encoded json
+            let newSrc = getBytesizedChunks(svg.encoded, 64, false)
+            newSrc.splice(0,0,`data:image/svg+xml;base64,`);
+
+            // Create new properties object
+            let newProps = {};
+
+            Object.keys(md.about.properties).map(key => {
+                let value = md.about.properties[key];
+                let newValue = null;
+
+                if (Array.isArray(value) && value.length === 1) {
+                    newValue = value[0];
+                } else {
+                    newValue = value;
+                }
+
+                if (key === `facial_hair`) {
+                    newProps[`Facial Hair`] = md.about.properties.facial_hair;
+                } else {
+                    newProps[key.charAt(0).toUpperCase() + key.slice(1)] = newValue;
+                }
+            });
+
+            let finalJson = {
+                name: `CryptoPeep #${aN}`,
+                artist: "Benzega",
+                description: "One-of-a-kind pop art pixel peep living on the blockchain!",
+                homepage: "https://www.cryptopeeps.io",
+                Properties: newProps,
+                image: newSrc,
+                mediaType: "image/svg+xml"
+            }
+
+            if (opts.test) {
+                console.log(aN);
+                inspect(finalJson);
+                fs.writeFileSync(__dirname + `/test/final.json`, JSON.stringify({
+                    "721": {
+                        "15b8520fae0359f40249c3c70ba967dbde925ae375573316d5b50c20": {
+                            "CryptoPeep1": finalJson
+                        },
+                        "version": "1.0"
+                    }
+                }));
+            } else {
+                console.log(aN);
+
+                // Clean dir
+                fs.rmdirSync(dir + `/${aN}`, { recursive: true });
+                fs.mkdirSync(dir + `/${aN}`, { recursive: true });
+
+                // Write files
+                fs.writeFileSync(dir + `/${aN}/artwork.svg`, svg.base);
+                fs.writeFileSync(dir + `/${aN}/${opts.title}`, JSON.stringify(finalJson));
+            }
+        }
+    });
+}
+
 /** ====================================================================================
  * Execution
  *
@@ -244,12 +384,22 @@ const createArtworkMetadata = (
 //     }
 // )
 
-// ! New artwork meta
-createArtworkMetadata(
-    'human',
-    'normal', {
-        title: "artwork.final.json",
-        test: true,
-        range: [1, 1]
-    }
-);
+// ! Create New artwork meta
+// createArtworkMetadata(
+//     'human',
+//     'normal', {
+//         title: "artwork.final.json",
+//         test: true,
+//         range: [1, 1]
+//     }
+// );
+
+// ! Create Final artwork meta
+// createFinalArtworkMetadata(
+//     'human',
+//     'normal', {
+//         title: "artwork.metadata.json",
+//         test: false,
+//         range: [1, 1]
+//     }
+// );
